eae1cd441f42a61d63325bf98f4e6fb3126bf3b6
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-03-15 08:29:41 +0100

    finish default function parameter

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("AHoy TypeScript!");
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(5, 25));
+
+//defining a parameter with a default value
+function multiplyWithDefault(a, b = 25) {
+  return a * b;
+}
+
+console.log(multiplyWithDefault(5));
+//overidding of a defaut parameter
+console.log(multiplyWithDefault(5, 35));
+
+/* corresponding classical implementation of a defaut value
+function multiplyWithDefault(a, b) {
+  if (!b) {
+    b = 25;
+  }
+  return a * b;
+}
+*/
