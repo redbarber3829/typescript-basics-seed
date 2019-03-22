b2a21a8a0c809ecf1169f5ac9c14ca8198092c21
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-03-22 08:44:03 +0100

    finish ES object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,48 @@
-console.log("AHoy TypeScript!");
+const pizza = {
+  name: "Pepperoni",
+  price: 15
+};
+
+const toppings = ["pepperoni"];
+
+//old way of declaring a new object based on two object
+const order = {
+  pizza: pizza,
+  toppings: toppings
+};
+
+//in ES6 or TS this is the corresponding implementation
+const orderES6 = { pizza, toppings };
+//create a property on an object based on a value of other property (the property name is defined based on the variable name)
+
+console.log(order);
+console.log(orderES6);
+
+//this syntax also works in function
+function createOrder(pizza, toppings) {
+  //return an object with property toppings and pizza
+  return { pizza, toppings };
+}
+
+console.log(createOrder(pizza, toppings));
+
+//old way of defining a function property in object
+/*const pizzaWithFunctionProperty = {
+  name: "Pepperoni",
+  price: 15,
+  getName: function() {
+    return this.name;
+  }
+};*/
+
+//using object litteral improvement to declare the function : its not necessary to declare the property name
+// the property name is created based on the function name
+const pizzaWithfunctionProperty = {
+  name: "Pepperoni",
+  price: 15,
+  getName() {
+    return this.name;
+  }
+};
+
+console.log(pizzaWithfunctionProperty.getName());
