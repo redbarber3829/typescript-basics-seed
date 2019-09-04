2b9eb1f07811182b427487bea76ba0b6a5944fac
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-04 17:51:16 +0200

    finishting special type void type

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("AHoy TypeScript!");
+//the void type doesn't exist in javascript language
+//void means there is no type at all
+
+let selectedTopping: string = 'pepperoni';
+
+/*-----------------------------------------------------------------*/
+//define a function that doesn't return anything
+//the void keyword will be use only a function
+/*-----------------------------------------------------------------*/
+
+function selectTopping(topping: string): void {
+  selectedTopping = topping;
+}
+//this not a pure function:
+//in a pure function you don't change a value of a function that is define outside the function
+
+selectTopping('bacon');
+
+console.log(selectedTopping);
