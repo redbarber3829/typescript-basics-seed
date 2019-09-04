8cb3bb65c0079311437a350f64fc5b82d9db56e6
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-04 12:35:21 +0200

    finish es array spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("AHoy TypeScript!");
+const toppings = ['bacon', 'chilli'];
+const newToppings = ['pepperoni'];
+
+/*old way of doing 
+If we want to merge all toping we would use concat method*/
+
+/*const allToppings = toppings.concat(newToppings);*/
+
+/*
+using the array spead operator
+the array spread operator is a kind of the reverse of the rest operator
+while the rest operator transform a list of argument in a single parameter of type array
+the array spread operator tranform the array in a list of argument
+*/
+
+const allToppings = [...toppings, ...newToppings];
+/* it doesn't keep a reference to the topping and the new toppings
+it will create a copy and put all the value in a new array
+that means that if you change a value in allTopping, we won't modify
+the value in the toppings or new toppings array*/
+
+console.log (allToppings);
