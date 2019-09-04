6ae02a5482be43040163bfaa1aae286245334fa2
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-04 16:39:18 +0200

    finishing primitive type boolean

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("AHoy TypeScript!");
+const pizzas: number = 5;
+
+/*----------------------------------------------------------*/
+// define a function that determine if a discount must be apply
+//based on the number of pizza
+/*----------------------------------------------------------*/
+
+function offerDiscount(orders: number): boolean {
+  return orders >= 3;
+}
+
+if (offerDiscount(pizzas)){
+  console.log(`You're entitled to a discount!`);
+} else {
+  console.log(`Order more than 3 pizzas for a discount!`);
+}
