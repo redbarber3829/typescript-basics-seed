5eebdba5aadfc6387010cdb9ab05e898f966efd4
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-05 11:56:59 +0200

    finishing special types functions and optional arguments

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("AHoy TypeScript!");
+//define a variable of type function 
+/*let sumOrder: (price: number, quantity: number) => number;
+
+//variable assignment
+sumOrder = (x, y) => x * y;
+
+//we want the second parameter of the function to be optional
+const sum = sumOrder(25);
+
+console.log(`Total sum: ${sum}`);*/
+
+/*---------------------------------------------------------------*/
+//we specify that a parameter is optional in the function type declaration using ?
+/*---------------------------------------------------------------*/
+
+let sumOrder: (price: number, quantity?: number) => number;
+
+//variable assignment
+sumOrder = (x, y) => {
+    //implement the logic in the function if the parameter quantity is undefined
+    if (y){
+        return x * y;
+    }
+    return x;  
+}
+
+//we want the second parameter of the function to be optional
+const sum = sumOrder(25);
+
+console.log(`Total sum: ${sum}`);
