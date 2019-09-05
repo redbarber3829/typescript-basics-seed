ac64d1dfac61981822a41573a4849979c2ec4b1a
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-05 12:06:03 +0200

    finishing special types typed functions and default params

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,34 @@
-//define a variable of type function 
-/*let sumOrder: (price: number, quantity: number) => number;
 
-//variable assignment
-sumOrder = (x, y) => x * y;
-
-//we want the second parameter of the function to be optional
-const sum = sumOrder(25);
-
-console.log(`Total sum: ${sum}`);*/
-
-/*---------------------------------------------------------------*/
-//we specify that a parameter is optional in the function type declaration using ?
-/*---------------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+// first solution to assign a default value
+// the default value is defined in the function implementation
+/*-----------------------------------------------------------*/
 
 let sumOrder: (price: number, quantity?: number) => number;
 
 //variable assignment
 sumOrder = (x, y) => {
-    //implement the logic in the function if the parameter quantity is undefined
-    if (y){
-        return x * y;
-    }
-    return x;  
+    const q = y || 1; // if y is not defined we assigned the quantity to 1
+    return x * q;  
 }
 
 //we want the second parameter of the function to be optional
 const sum = sumOrder(25);
 
 console.log(`Total sum: ${sum}`);
+
+/*-----------------------------------------------------------*/
+// second solution using the default params in the function definition
+/*-----------------------------------------------------------*/
+
+let sumOrderbis: (price: number, quantity?: number) => number;
+
+//variable assignment with a function having a default params
+sumOrderbis = (x, y = 1) => {
+    return x * y;  
+}
+
+//we want the second parameter of the function to be optional
+const sumbis = sumOrderbis(25);
+
+console.log(`Total sum: ${sumbis}`);
