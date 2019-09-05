f1d6958e668608e41ae36161abed60077b193193
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-05 16:53:20 +0200

    finishing interfaces with function types

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,41 @@
-//we will transform a custom type based on an alias to an interface
-
-/*-------------------------------------------------------------------------- */
-//using the alias type
-/*-------------------------------------------------------------------------- */
-
-/*type Pizza = {
-    name: string;
-    sizes: string[];
-};
-
-let pizza: Pizza;
-
-//here we emulate a creation of a pizza just like would do a server call
-function createPizza(name: string, sizes: string[]) {
-    return {
-        name,
-        sizes
-    };
-}
-//we suppose that is a sort of asynchonous request
-
-pizza = createPizza('Pepperoni',['small','medium']);
-
-//we are not sure that the create pizza return a pizza
-//we can add a restriction to the function signature by defining the return type
-//function createPizza(name: string, sizes: string[]) : Pizza {
-//    return {
-//        name,
-//        sizes
-//    };
-//}
-*/
-/*-------------------------------------------------------------------------- */
-//interface is the prefered approach while dealing with complex datastructure
-//interface define a contract
-//interface are more powerful than custom type as we can extend an interface (oject programming)
-/*-------------------------------------------------------------------------- */
-
 interface Pizza  {
     name: string;
     sizes: string[];
+    getAvailableSizes(): string[];
 };
 
 let pizza: Pizza;
 
-//here we emulate a creation of a pizza just like would do a server call
-function createPizza(name: string, sizes: string[]) : Pizza {
+function createPizza(name: string, sizes: string[]) : Pizza  {
     return {
         name,
-        sizes
+        sizes,
+        getAvailableSizes() {
+            return this.sizes;
+        }
     };
 }
-//we suppose that is a sort of asynchonous request
+
+/*-----------------------------------------------------------
+we can also use the assertion type to let typescript infered the type 
+of return
+
+function createPizza(name: string, sizes: string[])  {
+    return {
+        name,
+        sizes,
+        getAvailableSizes(){
+            return this.sizes;
+        }
+    } as Pizza;
+}
+it's problably more readable to define the return type directly in 
+the function signature
+-----------------------------------------------------------*/
 
 pizza = createPizza('Pepperoni',['small','medium']);
 
-//interface is a type that allow us to define the structure or shape of a particular object
 
-/*--------------------------------------------------------------------------------*/
-//we can combine different interfaces
-/*--------------------------------------------------------------------------------*/
-
-interface Pizzas {
-    data: Pizza[]
-}
+//notice that the function declaration as a custom type is little different than
+// a function declaration in an interface
+type getAvailableSizes = () => string[];
