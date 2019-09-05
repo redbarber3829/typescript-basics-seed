cbeb8d12fe3ce4333be4317a0ac7137d54f6e853
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-05 11:26:54 +0200

    finishing union and literal types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("AHoy TypeScript!");
+/*--------------------------------------------------------------- */
+// string literal type 
+/*--------------------------------------------------------------- */
+let pizzaSize: string = 'small';
+
+//the literal type allow us to define the authorized value that can be assigned to a string
+function selectSize(size: 'small' | 'medium' | 'large'): void {
+    pizzaSize = size;
+}
+
+//here we make a mistake in the spelling
+selectSize('meduim');
+
+console.log(`Pizza size: ${pizzaSize}`);
+
+
+/*--------------------------------------------------------------- */
+// number literal type 
+/*--------------------------------------------------------------- */
+let pizzaSize: number = 1;
+
+//the literal type allow us to define the authorized value that can be assigned to a string
+function selectSize(size: 1 | 2 | 3): void {
+    pizzaSize = size;
+}
+
+//here we make a mistake in the spelling
+selectSize(4);
+
+console.log(`Pizza size: ${pizzaSize}`);
