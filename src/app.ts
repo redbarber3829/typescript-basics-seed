f038a69a1a3ff408c1da82368a966a3940d360cf
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-05 10:39:33 +0200

    finishing special type never type

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("AHoy TypeScript!");
+// the never type is telling typescript compiler that the return value will never occur
+function orderError(error: string) : never {
+    throw new Error(error);
+    // here never going to return a value
+}
+
+orderError('Someting went wrong');
+
+//never type can be used to insure that there is no return that there is unreachable code
+// as the same idea: the code after while true loop will never be reached
