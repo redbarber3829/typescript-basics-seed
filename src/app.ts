355f8b356cd1f422f936dc4c741e25ddacaef311
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-05 12:44:56 +0200

    finishing special type array type and generic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("AHoy TypeScript!");
+const sizes = ['small', 'medium', 'large'];
+//when we assing the array to the variable we wan see the implicit type of the variable string[]
+
+/*-------------------------------------------------------------------------*/
+//explicit declaration of a string array type
+/*-------------------------------------------------------------------------*/
+
+let sizes2: string[];
+
+sizes2 = ['small', 'medium', 'large'];
+
+/*-------------------------------------------------------------------------*/
+//explicit declaration of a number array type
+/*-------------------------------------------------------------------------*/
+let sizes3: number[];
+
+sizes3 = [1, 2, 3];
+
+
+/*-------------------------------------------------------------------------*/
+//use of a generic type
+/*-------------------------------------------------------------------------*/
+//as we can declare an array using either [] or new Array
+//we can use the constructor type Array to use the generic
+
+let toppings: Array<string>;
+
+toppings = ['pepperoni','tomato','bacon'];
+
