58c8aebf3a3afc49aeab393436f29e2ec8f4b847
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-06 15:21:27 +0200

    finishing protected members and inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
-
-/**the purpose here is to avoid to create an instance of a Sizes class 
- * we can use abstract in order to avoid creating a new instance of a classe
- * avoid to call new Sizes(['small']);
-*/
-
-//we use the abstract keyword in order to create an abstract class
-
+/*
+//if we make the property sizes private in base class Sizes
 abstract class Sizes {
-    constructor(public sizes: string[]){}
+    constructor(private sizes: string[]){}
 
     set availableSizes(sizes: string[]){
         this.sizes = sizes;
@@ -22,7 +16,10 @@ abstract class Sizes {
 class Pizza extends Sizes {
     public toppings: string[] = [];
     
-    constructor(readonly name: string, public sizes: string[]){
+    //here we don't specify the accessibility of the sizes property
+    //if we specify it as public in the inherited we have a conflict with
+    //the accessibility define in the base class which is more restricted
+    constructor(readonly name: string, sizes: string[]){
         super(sizes);
     }
 
@@ -32,12 +29,54 @@ class Pizza extends Sizes {
 }
 
 const pizza = new Pizza('Pepperoni',['small', 'medium']);
-console.log(pizza.availableSizes);
+//here we can't call access to the propery sizes as 
+console.log(pizza.sizes);
 pizza.addTopping('pepperoni');
 console.log(pizza.name);
+*/
 
-//we can export the abstract class in order to be used in a different file
-//export abstract class Sizes {
+/*------------------------------------------------------------------------------*/
+//the purpose of the protected is to prevent accessing the property on an instance of 
+//a class like  this console.log(pizza.sizes);
+//but allow us to acces the property inside the inherited class
+/*------------------------------------------------------------------------------*/
 
- // if we try to instantiate an abstract class  new Sizes(['small']);
- //we have a message telling that we can't instantiate an abstract class
+
+abstract class Sizes {
+    //define the property sizes as protected
+    constructor(protected sizes: string[]){}
+
+    set availableSizes(sizes: string[]){
+        this.sizes = sizes;
+    }
+
+    get availableSizes(){
+        return this.sizes;
+    }
+
+}
+
+class Pizza extends Sizes {
+    public toppings: string[] = [];
+    
+    constructor(readonly name: string, sizes: string[]){
+        super(sizes);
+    }
+
+    //add a method that update the sizes property of the base class
+    public updateSizes(sizes: string[]){
+        //here we need to access the sizes property
+        //as long as the property sizes is private, it is only accessible within class 'Sizes'
+        //the protected accessibility allow us to access the property in the inherited class
+        this.sizes = sizes;
+    }
+
+    public addTopping(topping: string){
+        this.toppings.push(topping);
+    }
+}
+
+const pizza = new Pizza('Pepperoni',['small', 'medium']);
+console.log(pizza.availableSizes);
+pizza.updateSizes(['large']);
+console.log(pizza.availableSizes);
