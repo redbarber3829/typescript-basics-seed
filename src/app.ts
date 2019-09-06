f14c742c7f0bc0f9b93180085cc9761b4cf224a8
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-06 11:36:03 +0200

    finishing readonly members

--- src/app.ts
+++ src/app.ts
@@ -1,57 +1,30 @@
-//on javascript, there's no private members by defaut
-//using typescript we can have this behaviour
-//if we try to access something that we consider as private we will throw an error
-//private is something internal in a class that we don't want to access
+//the readonly property allow us to only read the property
+//can't modify the property
 /*class Pizza {
+    public toppings: string[] = [];
     
-    name: string;//this property is public by default
-    toppings: string[] = [];//this property is public by default
-    
-    constructor(name: string){
-        this.name = name;
-    }
+    constructor(public name: string){}
 
-    //this method is public
-    addTopping(topping: string){
+    public addTopping(topping: string){
         this.toppings.push(topping);
     }
 }
 
 const pizza = new Pizza('Pepperoni');
 pizza.addTopping('pepperoni');
+console.log(pizza.name);// here we can access the property both on read or on write
+console.log(pizza);
+*/
 
-console.log(pizza);*/
+/*----------------------------------------------------------------------------------*/
+//    if we only want to access the property name on read
+/*----------------------------------------------------------------------------------*/
 
-/*------------------------------------------------------------------------*/
-//by defaut, the properties or method attached to a class are public
-//this is equivalent to
-/*class Pizza {
-    public name: string;
-    public toppings: string[] = [];
-    
-    constructor(name: string){
-        this.name = name;
-    }
-
-    public addTopping(topping: string){
-        this.toppings.push(topping);
-    }
-}*/
-
-//if we want the method addTopping to be internal, we 
-// need to declare it like this
-/*private addTopping(topping: string){
-   this.toppings.push(topping);
-}*/
-
-/*------------------------------------------------------------------------*/
-//we can define a property private and affect it on the constructor using
 class Pizza {
     public toppings: string[] = [];
     
-    constructor(private name: string){}
+    constructor(readonly name: string){}
 
-    //this method is public
     public addTopping(topping: string){
         this.toppings.push(topping);
     }
@@ -59,19 +32,6 @@ class Pizza {
 
 const pizza = new Pizza('Pepperoni');
 pizza.addTopping('pepperoni');
-//here we can't access pizza.name
-
-console.log(pizza);
-
-//constructor(private name: string){} 
-//
-//is equivalent to
-//
-//private name: string;
-//constructor(name: string){
-//    this.name = name;
-//}
-
-//it also work with a public property
-//constructor(public name: string){}
-
+console.log(pizza.name);// here we can access the property both on read or on write
+pizza.name = 'ABC' //we can't assign a value to a readonly property
+// a readonly property can't be modified after its initialisation by the constructor
