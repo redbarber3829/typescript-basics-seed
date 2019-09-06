2518078a0c17cc1073797b3d002b12f866d18b9d
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-06 16:04:24 +0200

    finishing static properties and methods

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("AHoy TypeScript!");
+const date = new Date();
+//this is an instance methode as we instanciate an object above in order to call the method
+date.getFullYear();
+
+const date2 = Date.now();
+//this is a static method (we don't instantiate a new Date)
+
+
+/*----------------------------------------------------------------------*/
+//how can we create static properties and methods
+/*----------------------------------------------------------------------*/
+
+class Coupon {
+    //define a static property
+    static allowed  = ['Pepperoni', 'Blazing inferno'];
+    //define a static method
+    static create(percentage: number){
+        return `PIZZA_RESTAURANT_${percentage}`;
+    }
+}
+
+console.log(Coupon.allowed);
+console.log(Coupon.create(25));
+
+//static are useful when we define utility libraries
