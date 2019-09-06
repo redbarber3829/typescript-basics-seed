efae91c065918384076b006ee4629ef4f2b4cc9d
redbarber3829 <48354152+redbarber3829@users.noreply.github.com>
2019-09-06 12:17:07 +0200

    finishing class inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,35 @@
-//setter and getter allow us to control internally how a property can be set
 
 class Sizes {
     constructor(public sizes: string[]){}
 
-    //setter
     set availableSizes(sizes: string[]){
-        //we can implement a custom logic here
         this.sizes = sizes;
     }
 
-    //getter
     get availableSizes(){
-        //we can implement a logic here
-        return this.sizes; //access an internal property
+        return this.sizes;
     }
 
 }
 
-const sizes = new Sizes(['small','medium']);
-
-// invoke getter
-console.log(sizes.availableSizes);
-// invoke setter
-sizes.availableSizes = ['medium', 'large'];
-
-console.log(sizes.availableSizes);
-
-class Pizza {
+//the pizza class inherit from the Sizes class
+class Pizza extends Sizes {
     public toppings: string[] = [];
     
-    constructor(readonly name: string){}
+    constructor(readonly name: string, public sizes: string[]){
+        //call the constructor of the base class
+        super(sizes);
+    }
 
     public addTopping(topping: string){
         this.toppings.push(topping);
     }
 }
 
-const pizza = new Pizza('Pepperoni');
+const pizza = new Pizza('Pepperoni',['small', 'medium']);
+//inherit the getter from the base class
+console.log(pizza.availableSizes);
 pizza.addTopping('pepperoni');
 console.log(pizza.name);
 
-//setter an getter are always public
-//setter and getter offer flexibility with the possibility to had custom logic while 
-//setting a property 
+//when a class extend an other class, it inherit the getter, setter, method
